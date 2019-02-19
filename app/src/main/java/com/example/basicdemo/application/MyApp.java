58fd5d9ce5a7ba1b979ebefc31c141464589f9fe
balakrishna <balub513@gmail.com>
2019-02-19 23:36:25 +0530

    added Github commads link in Application file

--- app/src/main/java/com/example/basicdemo/application/MyApp.java
+++ app/src/main/java/com/example/basicdemo/application/MyApp.java
@@ -6,6 +6,8 @@ import com.google.firebase.FirebaseApp;
 
 public class MyApp extends Application {
 
+    // GIT Commands
+    //https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
     @Override
     public void onCreate() {
         super.onCreate();
